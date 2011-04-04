57388627535b15868753c3349dd7ada50c97c15c
Stephen Walker <stephenwalker@Stephen-Walkers-MacBook-Pro.local>
2011-04-04 15:11:40 -0500

    Added login + administrator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  before_filter :authorize
+	protect_from_forgery
 
 	private
 
@@ -9,6 +10,14 @@ class ApplicationController < ActionController::Base
 		cart = Cart.create
 		session[:cart_id] = cart.id
 		cart
+	end
+
+	protected
+
+	def authorize
+		unless User.find_by_id(session[:user_id])
+			redirect_to login_url, :notice => "Please log in"
+		end
 	end
 
 end
